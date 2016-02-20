fe9f07c01ad7f16ac6d9352f11cf1b1555aa31fe
ily9000 <ily9000@gmail.com>
2016-02-20 06:04:52 -0500

    images for mean and stdev

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+Here are the means of the pixels for each digit:
+![](images/NB_means.png?raw=true)
+
+
+Here are the standard deviations of the pixels for each digit:
 ![](images/NB_stdev.png?raw=true)
--- run_naiveBayes.py
+++ run_naiveBayes.py
@@ -5,6 +5,17 @@ import logging
 import naive_bayes as nb
 import load_data
 
+def plot_params(param, fname):
+    fig, axes = plt.subplots(1,10)
+    for i, ax in enumerate(axes):
+        ax.tick_params(labelleft='off',labelbottom='off')
+        im = ax.imshow(param[i].reshape(28,28))
+
+    cbar_ax = fig.add_axes([0.33, 0.4, 0.3, 0.03])
+    fig.colorbar(im, cax=cbar_ax, orientation = 'horizontal')
+    plt.rcParams["figure.figsize"] = [25,15]
+    plt.savefig(fname, bbox_inches='tight')
+
 def main():
 	
 	data = load_data.digits('')
@@ -16,14 +27,16 @@ def main():
 
 	logging.info('classifier is trained')
 	
-	digitsNB_clf.predict(data.values)
+	digitsNB_clf.predict(data.test_X)
 	logging.info('done predicting')
 	
 	pred_error = np.mean(data.test_Y == digitsNB_clf.predictions)
 	print pred_error
 
-	digitsNB_clf.predict_to_file('predictions.csv')
-	logging.info('done writing predicitons to file')
+	plot_params(digitsNB_clf.stdev, 'images/NB_stdev.png')
+	plot_params(digitsNB_clf.means, 'images/NB_means.png')
+	#digitsNB_clf.predict_to_file('predictions.csv')
+	#logging.info('done writing predicitons to file')
 
 if __name__ == '__main__':
 	logging.basicConfig(filename = 'nb_model.log', filemode = 'w', level = logging.DEBUG)
